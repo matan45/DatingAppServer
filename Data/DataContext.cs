33ad291753db24f84b2192c5e568bdfc5a027f5e
matan <matan33214@gmail.com>
2020-09-03 09:48:16 +0300

    3.9

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -15,5 +15,24 @@ namespace DatingApp.Data
         public DbSet<Value> Values { set; get; }
         public DbSet<User> Users { set; get; }
         public DbSet<Photo> Photos { set; get; }
+        public DbSet<Like> Likes { set; get; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Like>()
+                 .HasKey(k => new { k.LikerId, k.LikeeId });
+
+            builder.Entity<Like>()
+               .HasOne(u => u.Likee)
+               .WithMany(u => u.Likers)
+               .HasForeignKey(u => u.LikeeId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<Like>()
+               .HasOne(u => u.Liker)
+               .WithMany(u => u.Likees)
+               .HasForeignKey(u => u.LikerId)
+               .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
